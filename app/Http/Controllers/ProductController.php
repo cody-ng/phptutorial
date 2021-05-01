7e29de60c0129c2aa28fbbf8db62d49809fc5f87
Cody Ng <cng@vitralogy.com>
2021-05-01 18:35:41 -0400

    Update ProductController.php
    added validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -25,13 +25,25 @@ class ProductController extends Controller
 
      public function create(Request $request)
      {
+        $this->validate($request, [
+           'name' => 'required|alpha',
+           'price' => 'required|integer',
+           'description' => 'string|nullable'
+        ]);
+
+        /* 1) one way to create
        $product = new Product;
        $product->name= $request->name;
        $product->price = $request->price;
        $product->description= $request->description;
        
-       $product->save();
-       return response()->json($product);
+       $product->save();*/
+
+       // 2) another way to create
+       //    (make sure to set the model's "fillable" property)
+       $author = Product::create($request->all());
+
+       return response()->json($product, 201);
      }
 
      public function show($id)
@@ -42,20 +54,35 @@ class ProductController extends Controller
 
      public function update(Request $request, $id)
      { 
+        $this->validate($request, [
+           'name' => 'required|alpha',
+           'price' => 'required|integer',
+           'description' => 'string|nullable'
+        ]);
+        /* // 1) one way to update
         $product= Product::find($id);
         
         $product->name = $request->input('name');
         $product->price = $request->input('price');
         $product->description = $request->input('description');
         $product->save();
-        return response()->json($product);
+        */
+
+        // 2) another way to update
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+
+
+        return response()->json($product, 200);
      }
 
      public function destroy($id)
      {
-        $product = Product::find($id);
+        //$product = Product::find($id);
+        $product = Product::findOrFail($id);
+        
         $product->delete();
-        return response()->json('product removed successfully');
+        return response()->json('product removed successfully', 200);
      }
    
 }
